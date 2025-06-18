965c3c5d2ce242b2f13198dc3c658379197fbb7a
hayaf3254 <poso42731427@gmail.com>
2025-06-19 00:07:07 +0900

    Set up FastAPI with MySQL and Docker

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@db:3306/mydb"
+import os
+from dotenv import load_dotenv  # ← 追加
+
+# .envファイル読み込み
+load_dotenv()
+
+# 環境変数からDB情報を取得
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+# 接続URL構築
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
